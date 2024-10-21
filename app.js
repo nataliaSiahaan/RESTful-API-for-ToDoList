11d2a60b2174ca22649172c1e2abe26c15a0877f
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-10-21 21:09:38 +0700

    perbaiki kode login dan register user, dan melakukan test didalam thunder client berhasil

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express'); 
 const app = express(); 
+const PORT = process.env.PORT || 3000;
+
 const bodyParser = require('body-parser'); 
 const dotenv = require('dotenv'); 
 const { sequelize } = require('./models'); 
 
 dotenv.config(); 
-
 app.use(bodyParser.json()); 
 
 // Import routes
@@ -15,8 +16,7 @@ const todoRoutes = require('./routes/todo');
 // Gunakan routes
 app.use('/api/users', userRoutes); 
 app.use('/api/todos', todoRoutes); 
-
-const PORT = process.env.PORT || 5000; 
+ 
 
 app.listen(PORT, async () => { 
     console.log(`Server is running on port ${PORT}`); 
--- controllers/userController.js
+++ controllers/userController.js
@@ -4,25 +4,31 @@ const { User } = require('../models');
 
 // Register Membuat user baru 
 exports.register = async (req, res) => {
-  const { name, email, password } = req.body;
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hashedPassword });
-    res.status(201).json({ message: 'User created successfully.' });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+    const { name, email, password } = req.body;  // Mengambil data dari permintaan
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);  // Mengacak password
+        const user = await User.create({ name, email, password: hashedPassword });  // Menyimpan user di database
+        res.status(201).json({ message: 'User created successfully.' });  // Berhasil mendaftarkan user
+    } catch (err) {
+        res.status(400).json({ message: err.message });  // Jika ada error
+    }
 };
+
+
+  
 
 // Login
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
+    // Cek apakah pengguna ada
     const user = await User.findOne({ where: { email } });
-    if (!user) return res.status(400).json({ message: 'Invalid email or password.' });
+    console.log('User Found:', user); // Cetak pengguna yang ditemukan
+
+    if (!user) return res.status(400).json({ message: 'Invalid email or password.' });// Jika email tidak ditemukan
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Invalid email or password.' });
+    if (!isMatch) return res.status(400).json({ message: 'Invalid email or password.' });// Jika password tidak ditemukan
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
     res.json({ token });
